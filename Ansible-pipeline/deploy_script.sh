6c819b78155cf88d28c131c0566d29dab96c5e28
Or <134198819+Orelbaz@users.noreply.github.com>
2023-08-15 10:58:39 +0300

    Delete Ansible-pipeline directory

--- Ansible-pipeline/deploy_script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo yum install python -y
-sudo yum install python-pip -y
-sudo pip install ansible
-ansible-playbook /home/ec2-user/Jenkins/Ansible-pipeline/ansible/flask-playbook.yml 
